a9327d70ace582947760d61c3b4757fa24ad062b
aion0721 <kobayashi@test.com>
2020-05-12 09:05:55 +0900

    Make DebugMes

--- src/main/java/com/example/sandbox/DemoApplication.java
+++ src/main/java/com/example/sandbox/DemoApplication.java
@@ -3,11 +3,14 @@ package com.example.sandbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import software.amazon.codeguruprofilerjavaagent.Profiler;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("tst");
 	}
 
 }
